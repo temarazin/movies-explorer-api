bf2a1abcb8a3f952e0bcaa3c4b5a3c4f4f498231
temarazin <temarazin@gmail.com>
2022-07-24 13:23:04 +0300

    fix: fix database name

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 router(app);
 
