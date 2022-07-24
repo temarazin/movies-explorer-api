d3d4bf79ad027d21d0b382d4dafe812dcf73a884
temarazin <temarazin@gmail.com>
2022-07-24 12:43:40 +0300

    feat: add default messages for Error classes

--- classes/Error.js
+++ classes/Error.js
@@ -3,35 +3,35 @@
 const { STATUS_CODE } = require('../utils/constants');
 
 module.exports.BadRequestError = class BadRequestError extends Error {
-  constructor(msg) {
+  constructor(msg = 'Некорректные данные в запросе') {
     super(msg);
     this.statusCode = STATUS_CODE.error.badRequest;
   }
 };
 
 module.exports.UnauthorizedError = class UnauthorizedError extends Error {
-  constructor(msg) {
+  constructor(msg = 'Необходима авторизация') {
     super(msg);
     this.statusCode = STATUS_CODE.error.unauthorized;
   }
 };
 
 module.exports.ForbiddenError = class ForbiddenError extends Error {
-  constructor(msg) {
+  constructor(msg = 'Доступ запрещен') {
     super(msg);
     this.statusCode = STATUS_CODE.error.forbidden;
   }
 };
 
 module.exports.NotFoundError = class NotFoundError extends Error {
-  constructor(msg) {
+  constructor(msg = 'Ничего не найдено') {
     super(msg);
     this.statusCode = STATUS_CODE.error.notFound;
   }
 };
 
 module.exports.ConflictError = class ConflictError extends Error {
-  constructor(msg) {
+  constructor(msg = 'Запрос не может быть исполнен') {
     super(msg);
     this.statusCode = STATUS_CODE.error.conflict;
   }
