d91beef3383eb3624e9e359b8a5f498b2b827ca0
temarazin <temarazin@gmail.com>
2022-07-29 13:28:29 +0300

    feat: add default route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const { NotFoundError } = require('../classes/Error');
 
 module.exports = (app) => {
   app.use('/users/', usersRouter);
   app.use('/movies/', moviesRouter);
+  app.use(() => {
+    throw new NotFoundError('Неверный путь');
+  });
 };
