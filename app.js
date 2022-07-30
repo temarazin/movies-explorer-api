a49f459c6feeb7ed3e3785dc7ca2812a4eefa493
temarazin <temarazin@gmail.com>
2022-07-30 07:21:59 +0300

    refactor: remove old temporary code

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(dbConnect.dbHost + dbConnect.dbName);
 
-// app.use((req, res, next) => {
-//   req.user = { _id: '62dd1d0bcd9d20ccdac599ee' };
-//   next();
-// });
-
 app.use(cookieParser());
 app.use(requestLogger);
 
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,5 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-const { getUser, updateUser, createUser } = require('../controllers/users');
-const MSG = require('../utils/messages');
-const { validation } = require('../utils/settings');
-
-// temp route
-router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string()
-      .$.min(validation.user.nameMinLength).max(validation.user.nameMaxLength)
-      .rule({ message: MSG.validation.user.invalidNameLength }),
-    email: Joi.string().email().message(MSG.validation.user.invalidEmail),
-    password: Joi.string(),
-  }),
-}), createUser);
+const { getUser, updateUser } = require('../controllers/users');
 
 router.get('/me', getUser);
 router.patch('/me', updateUser);
