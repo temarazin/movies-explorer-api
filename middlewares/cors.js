04ff7a8266d3b6a0397cf0c8c3adc5191a801a54
temarazin <temarazin@gmail.com>
2022-09-07 20:48:45 +0300

    fix: very small fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 const allowedCors = [
   'http://localhost:3000',
   'http://movie-explorer.temarazin.ru',
-  'https://movie-explorer.temarazin.ru/',
+  'https://movie-explorer.temarazin.ru',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
